7466387520542b111a6cbb8550f1882a98618988
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-11-20 15:43:54 +0900

    initial commit

---
+++ googleNews.py
@@ -0,0 +1,23 @@
+from GoogleNews import GoogleNews
+from newspaper import Article
+
+googlenews = GoogleNews(lang='en', encode='utf-8')
+googlenews.get_news('openai')
+result = googlenews.results()
+print(result)
+
+article_link = result[0]['link']
+
+if not article_link.startswith("https://"):
+    article_link = "https://" + article_link
+
+
+article = Article(article_link)
+article.download()
+article.parse()
+
+
+# # Print the article's title and summary
+# print("Title:", article.title)
+# print("\Text:")
+# print(article.text)
