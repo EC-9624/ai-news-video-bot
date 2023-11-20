50ab5091b69a86c3a29ef247467d7f655658a9a9
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-11-20 16:24:33 +0900

    add requirements and llm example

--- googleNews.py
+++ googleNews.py
@@ -1,11 +1,12 @@
 from GoogleNews import GoogleNews
 from newspaper import Article
 
-googlenews = GoogleNews(lang='en', encode='utf-8')
+
+googlenews = GoogleNews(lang='ja', encode='utf-8')
 googlenews.get_news('openai')
 result = googlenews.results()
+links = googlenews.get_links()
 print(result)
-
 article_link = result[0]['link']
 
 if not article_link.startswith("https://"):
@@ -17,7 +18,7 @@ article.download()
 article.parse()
 
 
-# # Print the article's title and summary
-# print("Title:", article.title)
-# print("\Text:")
-# print(article.text)
+# Print the article's title and summary
+print("Title:", article.title)
+print("\Text:")
+print(article.text)
